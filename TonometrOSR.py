e98b9a5fed8a32d7f1aa218e51f9a91dd2ab33a8
djp1-hub <69582216+djp1-hub@users.noreply.github.com>
2025-06-09 14:15:00 +0200

    working way

--- TonometrOSR.py
+++ TonometrOSR.py
@@ -4,19 +4,38 @@ import pytesseract
 import os
 
 class TonometerOCR:
-    def __init__(self, tessdata_dir, lang="ssd", psm=8):
+    def __init__(self, tessdata_dir, lang="ssd", psm=7):
         self.tessdata_dir = tessdata_dir
         self.lang = lang
         self.psm = psm
 
+
+
     def resize_to_width(self, image, width=100):
         h, w = image.shape[:2]
         scale = width / w
+
         new_dim = (width, int(h * scale))
         return cv2.resize(image, new_dim, interpolation=cv2.INTER_LINEAR)
 
     def invert(self, image):
         return cv2.bitwise_not(image)
+
+
+    def morf_closed(self, image):
+        img = cv2.imread("lcd_rectified.jpg")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+        gray_eq = clahe.apply(gray)
+        blurred = cv2.GaussianBlur(gray_eq, (5, 5), 0)
+        _, thresh = cv2.threshold(blurred, 100, 255, cv2.THRESH_BINARY_INV)
+
+        # === 2. Морфология для склейки сегментов в цифры ===
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 5))  # вертикально удлинённое ядро
+        closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+        cv2.imwrite("debug_morph_closed.jpg", closed)
+        return closed
+
 
     def extract_lcd_screen(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -54,7 +73,7 @@ class TonometerOCR:
         M = cv2.getPerspectiveTransform(rect, dst)
         warped = cv2.warpPerspective(image, M, (width, height))
 
-        #cv2.imwrite("lcd_rectified.jpg", warped)
+        cv2.imwrite("lcd_rectified.jpg", warped)
         print("[INFO] LCD дисплей выровнен и сохранён: lcd_rectified.jpg")
         return warped
 
@@ -84,9 +103,9 @@ class TonometerOCR:
             norm = cv2.addWeighted(row, 1.5, blur, -0.5, 0)
             resize = self.resize_to_width(norm, 65)
 
-            #filename = f"ocr_row_{i+1}.jpg"
-            #cv2.imwrite(filename, resize)
-            #print(f"[DEBUG] Сохранил {filename}")
+            filename = f"ocr_row_{i+1}.jpg"
+            cv2.imwrite(filename, resize)
+            print(f"[DEBUG] Сохранил {filename}")
 
             config = f"--psm {self.psm} -l {self.lang} --tessdata-dir {self.tessdata_dir}"
             text = pytesseract.image_to_string(resize, config=config)
@@ -103,7 +122,7 @@ class TonometerOCR:
             raise FileNotFoundError(f"Не удалось загрузить изображение: {image_path}")
 
         lcd = self.extract_lcd_screen(image)
-        #cv2.imwrite("lcd.jpg", lcd)
+        # cv2.imwrite("lcd.jpg", lcd)
 
         values = self.extract_values_from_lcd(lcd)
         if len(values) == 3:
@@ -111,3 +130,18 @@ class TonometerOCR:
         else:
             print("[WARN] Не все значения получены:", values)
         return values
+    def process_full(self, image_path):
+        image = cv2.imread(image_path)
+        if image is None:
+            raise FileNotFoundError(f"Не удалось загрузить изображение: {image_path}")
+        lcd = self.extract_lcd_screen(image)
+        morf_closed = self.morf_closed(lcd)
+        config = f"--psm 3 -l ssd --tessdata-dir tessdata"
+        text = pytesseract.image_to_string(morf_closed, config=config)
+        values = text.strip().splitlines()
+        print(values)
+        return values
+
+
+
+
